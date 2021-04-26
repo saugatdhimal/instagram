9848127b6ebb870dd3bc3a102d96d33d31d51c09
saugatdhimal <dhimalsaugat77@gmail.com>
2021-04-26 15:19:22 +0545

    firebase stuff,  seed js to fill firestore,  👊👍

---
+++ src/seed.js
@@ -0,0 +1,72 @@
+export function seedDatabase(firebase) {
+    const users = [
+      {
+        userId: 'IE0jwanaeRYArjfRaPihd5cOcEq2',
+        username: 'karl',
+        fullName: 'Karl Hadwen',
+        emailAddress: 'karlhadwen@gmail.com',
+        following: ['2'],
+        followers: ['2', '3', '4'],
+        dateCreated: Date.now()
+      },
+      {
+        userId: 'MxPMNmufvzNmmhvQbYscTQinwVu2',
+        username: 'raphael',
+        fullName: 'Raffaello Sanzio da Urbino',
+        emailAddress: 'raphael@sanzio.com',
+        following: [],
+        followers: ['IE0jwanaeRYArjfRaPihd5cOcEq2'],
+        dateCreated: Date.now()
+      },
+      {
+        userId: 'owvy45PXgUf7QoTbDPDqENRk3bU2',
+        username: 'dali',
+        fullName: 'Salvador Dalí',
+        emailAddress: 'salvador@dali.com',
+        following: [],
+        followers: ['IE0jwanaeRYArjfRaPihd5cOcEq2'],
+        dateCreated: Date.now()
+      },
+      {
+        userId: 'g88q4PE2WYXj6yfQbr5wDC6LFgT2',
+        username: 'orwell',
+        fullName: 'George Orwell',
+        emailAddress: 'george@orwell.com',
+        following: [],
+        followers: ['IE0jwanaeRYArjfRaPihd5cOcEq2'],
+        dateCreated: Date.now()
+      }
+    ];
+  
+    // eslint-disable-next-line prefer-const
+    for (let k = 0; k < users.length; k++) {
+      firebase.firestore().collection('users').add(users[k]);
+    }
+  
+    // eslint-disable-next-line prefer-const
+    for (let i = 1; i <= 5; ++i) {
+      firebase
+        .firestore()
+        .collection('photos')
+        .add({
+          photoId: i,
+          userId: '2',
+          imageSrc: `/images/users/raphael/${i}.jpg`,
+          caption: 'Saint George and the Dragon',
+          likes: [],
+          comments: [
+            {
+              displayName: 'dali',
+              comment: 'Love this place, looks like my animal farm!'
+            },
+            {
+              displayName: 'orwell',
+              comment: 'Would you mind if I used this picture?'
+            }
+          ],
+          userLatitude: '40.7128°',
+          userLongitude: '74.0060°',
+          dateCreated: Date.now()
+        });
+    }
+  }
