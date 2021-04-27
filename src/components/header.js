1da07d7fbb5574358d9439a7133ceab48518a34e
saugatdhimal <dhimalsaugat77@gmail.com>
2021-04-27 18:17:00 +0545

    header, ✍✍✍

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,88 @@
-import React, { useContext } from 'react'
-import FirebaseContext from '../context/firebase'
-import UserContext from '../context/user'
+import React, { useContext } from "react";
+import FirebaseContext from "../context/firebase";
+import UserContext from "../context/user";
+import { Link } from "react-router-dom";
+import * as ROUTES from "../constants/routes";
 
 function Header() {
-    const { firebase } = useContext(FirebaseContext);
-    const { user } = useContext(UserContext);
-    return (
-        <div>
-            <p>{user.displayName}</p>
+  const { firebase } = useContext(FirebaseContext);
+  const { user } = useContext(UserContext);
+  return (
+    <div className="h-14 bg-white border-b border-gray-primary mb-8">
+      <div className="container mx-auto max-w-screen-lg h-full">
+        <div className="flex justify-between h-full">
+          <div className="text-gray-700 text-center flex items-center align-items cursor-pointer">
+            <h1 className="flex justify-center w-full">
+              <Link to={ROUTES.DASHBOARD} aria-label="Instagram logo">
+                <img
+                  src="/images/logo.png"
+                  alt="Instagram logo"
+                  className="mt-2 w-6/12"
+                />
+              </Link>
+            </h1>
+          </div>
+          <div className="text-gray-700 text-center flex items-center">
+            {user ? (
+              <>
+                <Link to={ROUTES.DASHBOARD} aria-label="Dashboard">
+                  <svg
+                    className="w-7 mr-6 text-black-light cursor-pointer"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+                  </svg>
+                </Link>
+                <button
+                  className="font-bold text-sm"
+                  type="button"
+                  title="Sign Out"
+                  onClick={() => firebase.auth().signOut()}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      firebase.auth().signOut();
+                    }
+                  }}
+                >
+                  Sign Out
+                </button>
+                <div className="flex items-center cursor-pointer">
+                  <Link to={`/p/${user?.displayName}`}>
+                    <img
+                      className="rounded-full h-8 w-8 flex ml-6"
+                      src={`/images/avatars/dali.jpg`}
+                      alt={`${user?.displayName} profile`}
+                    />
+                  </Link>
+                </div>
+              </>
+            ) : (
+              <>
+                <Link to={ROUTES.LOGIN}>
+                  <button
+                    type="button"
+                    className="bg-blue-medium font-bold text-sm rounded text-white w-20 h-8"
+                  >
+                    Log In
+                  </button>
+                </Link>
+                <Link to={ROUTES.SIGN_UP}>
+                  <button
+                    type="button"
+                    className="font-bold text-sm rounded text-blue-medium w-20 h-8"
+                  >
+                    Sign Up
+                  </button>
+                </Link>
+              </>
+            )}
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
